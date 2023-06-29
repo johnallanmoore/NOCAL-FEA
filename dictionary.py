7f5bed88c85fb4a4bcbf47e90c316cc0cb4eb2d8
Chandel <ayushi.chandel@marquette.edu>
2023-06-29 13:19:51 -0500

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 11:55:50 2023
+
+@author: chandela
+"""
+
+thisdict = {
+    'neperInput' : 'paraFipMesh_48.inp',
+    'deckName' : 'paraFipMesh_11'
+    
+}
+
+
+print(thisdict)
